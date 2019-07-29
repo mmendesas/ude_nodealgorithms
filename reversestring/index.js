4fe8892413b85cfa1c2b7e68e4cbc87fa032251c
Marcio Mendes <mmendes.as@gmail.com>
2019-07-29 00:36:05 -0300

    initial commit with exercises

---
+++ reversestring/index.js
@@ -0,0 +1,28 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+    return str.split('').reduce((rev, char) => char + rev, '');
+}
+
+// 1 - solution using array reverse built in
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// 2 - solution using for
+// function reverse(str) {
+//     let reversed = '';
+//     for (let character of str) {
+//         reversed = character + reversed;
+//     }
+//     return reversed;
+// }
+
+
+module.exports = reverse;
