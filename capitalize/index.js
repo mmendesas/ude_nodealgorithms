548c01beac1698b57c879fb564cdc45b6212c0ad
Marcio Mendes <mmendes.as@gmail.com>
2019-08-06 00:40:38 -0300

    feat: add capitalize solution

--- capitalize/index.js
+++ capitalize/index.js
@@ -7,6 +7,33 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    let result = str[0].toUpperCase();
+    for (let i = 1; i < str.length; i += 1) {
+        if (str[i - 1] === ' ') {
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i]
+        }
+    }
+    return result;
+}
+
+
+// 1 solution with slice
+// function capitalize(str) {
+//     return str.split(' ')
+//         .map(item => item[0].toUpperCase() + item.slice(1))
+//         .join(' ')
+// }
+
+// 2 solution with for
+// function capitalize(str) {
+//     const words = [];
+//     for (let word of str.split(' ')) {
+//         words.push(word[0].toUpperCase() + word.slice(1));
+//     }
+//     return words.join(' ');
+// }
 
 module.exports = capitalize;
