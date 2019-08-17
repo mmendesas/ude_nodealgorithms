846749cc187d3af276eea05742e21276c48cdd38
Marcio Mendes <mmendes.as@gmail.com>
2019-08-17 14:32:26 -0300

    feat: add steps solution

--- steps/index.js
+++ steps/index.js
@@ -17,6 +17,28 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = '') {
+    if (n === row) {
+        return;
+    }
+
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+    const add = stair.length <= row ? '#' : ' ';
+    steps(n, row, stair + add);
+}
+
+// 1 solution with for iteration
+// function steps(n) {
+//     for (let row = 0; row < n; row++) {
+//         let stair = '';
+//         for (let col = 0; col < n; col++) {
+//             stair += col <= row ? '#' : ' ';
+//         }
+//         console.log(stair)
+//     }
+// }
 
 module.exports = steps;
