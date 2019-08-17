2c811fa90734d9babc4d7c00b1a80daa829ddf74
Marcio Mendes <mmendes.as@gmail.com>
2019-08-17 17:23:45 -0300

    feat: add spiral matrix solution

--- matrix/index.js
+++ matrix/index.js
@@ -15,6 +15,52 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    // create an array of arrays
+    const spiral = []
+    for (let i = 0; i < n; i++) {
+        spiral.push([]);
+    }
+    let count = 1;
+
+    let startCol = 0;
+    let endCol = n - 1;
+    let startRow = 0;
+    let endRow = n - 1;
+
+    while (startCol <= endCol && startRow <= endRow) {
+        // top row (left > right)
+        for (let i = startCol; i <= endCol; i += 1) {
+            spiral[startRow][i] = count;
+            count++;
+        }
+        startRow++;
+
+        // right collumn (up > down)
+        for (let i = startRow; i <= endRow; i++) {
+            spiral[i][endCol] = count;
+            count++;
+        }
+        endCol--;
+
+        // bottom row (right > left)
+        for (let i = endCol; i >= startCol; i -= 1) {
+            spiral[endRow][i] = count;
+            count++;
+        }
+        endRow--;
+
+        // start collumn (down > up)
+        for (let i = endRow; i >= startRow; i -= 1) {
+            spiral[i][startCol] = count;
+            count++;
+        }
+        startCol++;
+        console.table(spiral)
+    }
+
+    console.table(spiral)
+    return spiral;
+}
 
 module.exports = matrix;
