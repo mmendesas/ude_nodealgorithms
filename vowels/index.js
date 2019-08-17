abe7cb082926596dbcfef5de01bbff8af19e96f9
Marcio Mendes <mmendes.as@gmail.com>
2019-08-17 17:23:34 -0300

    feat: add vowels solution

--- vowels/index.js
+++ vowels/index.js
@@ -7,6 +7,25 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+}
+
+// 1 solution with regex
+// function vowels(str) {
+//     return str.replace(/[^aeiou]/gi, '').length;
+// }
+
+// 2 solution with forloop
+// function vowels(str) {
+//     let count = 0;
+//     for (let char of str) {
+//         if ('aeiou'.includes(char.toLowerCase())) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
 
 module.exports = vowels;
