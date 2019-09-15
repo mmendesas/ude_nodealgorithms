4a6114071d3d8c32343561c32c5744d29d1b9316
Marcio Mendes <mmendes.as@gmail.com>
2019-09-15 18:27:58 -0300

    feat: qfroms solution

--- qfroms/index.js
+++ qfroms/index.js
@@ -14,6 +14,40 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+	constructor() {
+		this.first = new Stack();
+		this.second = new Stack();
+	}
+
+	add(item) {
+		this.first.push(item);
+	}
+
+	remove() {
+		while (this.first.peek()) {
+			this.second.push(this.first.pop());
+		}
+		const last = this.second.pop();
+
+		while (this.second.peek()) {
+			this.first.push(this.second.pop());
+		}
+		return last;
+	}
+
+	peek() {
+		while (this.first.peek()) {
+			this.second.push(this.first.pop());
+		}
+		const item = this.second.peek();
+
+		while (this.second.peek()) {
+			this.first.push(this.second.pop());
+		}
+
+		return item;
+	}
+}
 
 module.exports = Queue;
