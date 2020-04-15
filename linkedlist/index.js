0970b84069a6d0cce86c81cc2bb3b1a2e0e7e86a
Marcio Mendes <mmendes.as@gmail.com>
2020-04-14 23:04:13 -0300

    feat: implemente Node and first methods of LinkedList

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -2,8 +2,54 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head)
+  }
+
+  size() {
+    let node = this.head;
+    let count = 0;
+
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    if (!this.head) {
+      return null;
+    }
+
+    let node = this.head;
+    while (node) {
+      if (!node.next) {
+        return node;
+      }
+      node = node.next
+    }
+  }
+
+  clear() {
+    this.head = null;
+  }
+}
 
 module.exports = { Node, LinkedList };
