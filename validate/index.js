94a2dcacd99d7756b4705379c026c7ad45f1658f
Marcio Mendes <mmendes.as@gmail.com>
2020-05-02 00:04:42 -0300

    feat(bst): validate binary search tree

--- validate/index.js
+++ validate/index.js
@@ -5,6 +5,25 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  if (max !== null && node.data > max) {
+    return false;
+  }
+  if (min !== null && node.data < min) {
+    return false;
+  }
+
+  // on the left update max value
+  if (node.left && !validate(node.left, min, node.data)) {
+    return false;
+  }
+
+  // on the right update min value
+  if (node.right && !validate(node.right, node.data, max)) {
+    return false;
+  }
+
+  return true;
+}
 
 module.exports = validate;
