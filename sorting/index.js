ec97856bc0d3577148cea5b784cbdef25f4cd1e4
Marcio Mendes <mmendes.as@gmail.com>
2020-05-05 01:22:35 -0300

    feat: solve merge two sorted arrays

--- sorting/index.js
+++ sorting/index.js
@@ -37,10 +37,19 @@ function selectionSort(arr) {
 
 function mergeSort(arr) {
 
+
 }
 
 function merge(left, right) {
-
+  let results = []
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift())
+    }
+  }
+  return [...results, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
